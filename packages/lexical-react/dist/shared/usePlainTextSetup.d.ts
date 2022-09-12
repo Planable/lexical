9bd06b80e77836f706e0753050268e8d0da28001
petru <petru@planable.io>
2022-09-12 14:57:52 +0300

    🚧 commit dist

---
+++ packages/lexical-react/dist/shared/usePlainTextSetup.d.ts
@@ -0,0 +1,13 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+import type {LexicalEditor} from 'lexical';
+import {InitialEditorStateType} from '@lexical/plain-text';
+export declare function usePlainTextSetup(
+  editor: LexicalEditor,
+  initialEditorState?: InitialEditorStateType,
+): void;
